bb38fde263a56c65909b3240ea547876ce478a98
Prashanth <yvprashanth@gmail.com>
2018-05-29 16:34:56 -0700

    Change the name from KeyVaultURI to VAULT_NAME

--- Program.cs
+++ Program.cs
@@ -15,7 +15,7 @@ namespace dotnetconsole
             Console.WriteLine("This DotNet Console Application authenticates to Key Vault!");
             Console.WriteLine("It also creates a Secret Key Value Pair!");
             Console.WriteLine("And then it gets the Secret Key Value Pair!");
-            var keyvaultUri = System.Environment.GetEnvironmentVariable("KEYVAULT_URI", EnvironmentVariableTarget.User);
+            var keyvaultUri = System.Environment.GetEnvironmentVariable("VAULT_NAME", EnvironmentVariableTarget.User);
             var APPLICATION_ID = System.Environment.GetEnvironmentVariable("APPLICATION_ID", EnvironmentVariableTarget.User);
             var CERT_THUMBPRINT = System.Environment.GetEnvironmentVariable("CERT_THUMBPRINT", EnvironmentVariableTarget.User);
             
