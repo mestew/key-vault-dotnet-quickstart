8827a5c8785dcd1c07caa95ee26ec714327f1539
Prashanth <yvprashanth@gmail.com>
2018-06-20 15:49:01 -0700

    Finally Got it to work

--- Util.cs
+++ Util.cs
@@ -14,7 +14,6 @@ public class Util
 {
     public static X509Certificate2 ConvertFromPfxToPem(string filename)
     {
-        AsymmetricCipherKeyPair sample;
         using (System.IO.FileStream fs = System.IO.File.OpenRead(filename))
         {
             byte[] data = new byte[fs.Length];
@@ -23,7 +22,6 @@ public class Util
             if (data[0] != 0x30)
             {
                 res = GetPem("CERTIFICATE", data);
-                // rsaKeyBits = GetPem("PRIVATE KEY", data);
             }
             X509Certificate2 x509 = new X509Certificate2(res); //Exception hit here
             return x509;
@@ -32,15 +30,6 @@ public class Util
 
     private static byte[] GetPem(string type, byte[] data)
     {
-        string base64 = GetSubstringFromCert(type, data);
-        base64 = base64.Replace('-', '+');
-        base64 = base64.Replace('_', '/');
-        return Convert.FromBase64String(base64);
-    }
-
-
-    private static string GetSubstringFromCert(string type, byte[] data)
-    {
         string pem = Encoding.UTF8.GetString(data);
         string header = String.Format("-----BEGIN {0}-----", type);
         string footer = String.Format("-----END {0}-----", type);
@@ -48,16 +37,9 @@ public class Util
         int end = pem.IndexOf(footer, start);
         string base64 = pem.Substring(start, (end - start));
         base64 = base64.Replace(System.Environment.NewLine, "");
-        return base64;
-    }
-
-    private static string ConvertToBase64String(string data)
-    {
-        X509Certificate2 certificate = ConvertFromPfxToPem("cert.pem");
-        RSACryptoServiceProvider rsaCsp = (RSACryptoServiceProvider)certificate.PrivateKey;
-        byte[] dataBytes = Encoding.UTF8.GetBytes(data);
-        byte[] signatureBytes = rsaCsp.SignData(dataBytes, "SHA1");
-        return Convert.ToBase64String(signatureBytes);
+        base64 = base64.Replace('-', '+');
+        base64 = base64.Replace('_', '/');
+        return Convert.FromBase64String(base64);
     }
 
     public static RSACryptoServiceProvider PemFileReader(){
@@ -82,85 +64,5 @@ public class Util
         RSACryptoServiceProvider rsaKey = new RSACryptoServiceProvider(2048);
         rsaKey.ImportParameters(rsaParameters);
         return rsaKey;
-    }
-
-    private static RSACryptoServiceProvider CreateRsaProviderFromPrivateKey(string privateKey)
-    {
-        var RSA = new RSACryptoServiceProvider();
-        try {
-            privateKey = privateKey.Replace('-', '+');
-            privateKey = privateKey.Replace('_', '/');
-            var privateKeyBits = System.Convert.FromBase64String(privateKey);
-            
-            var RSAparams = new RSAParameters();
-
-            using (BinaryReader binr = new BinaryReader(new MemoryStream(privateKeyBits)))
-            {
-                byte bt = 0;
-                ushort twobytes = 0;
-                twobytes = binr.ReadUInt16();
-                if (twobytes == 0x8130)
-                    binr.ReadByte();
-                else if (twobytes == 0x8230)
-                    binr.ReadInt16();
-                else
-                    throw new Exception("Unexpected value read binr.ReadUInt16()");
-
-                twobytes = binr.ReadUInt16();
-                if (twobytes != 0x0102)
-                    throw new Exception("Unexpected version");
-
-                bt = binr.ReadByte();
-                if (bt != 0x00)
-                    throw new Exception("Unexpected value read binr.ReadByte()");
-
-                RSAparams.Modulus = binr.ReadBytes(GetIntegerSize(binr));
-                RSAparams.Exponent = binr.ReadBytes(GetIntegerSize(binr));
-                RSAparams.D = binr.ReadBytes(GetIntegerSize(binr));
-                RSAparams.P = binr.ReadBytes(GetIntegerSize(binr));
-                RSAparams.Q = binr.ReadBytes(GetIntegerSize(binr));
-                RSAparams.DP = binr.ReadBytes(GetIntegerSize(binr));
-                RSAparams.DQ = binr.ReadBytes(GetIntegerSize(binr));
-                RSAparams.InverseQ = binr.ReadBytes(GetIntegerSize(binr));
-            }
-            RSA.ImportParameters(RSAparams);
-        } catch(Exception ex){
-            Console.WriteLine(ex.Message);
-        }
-        return RSA;
-    }
-
-    private static int GetIntegerSize(BinaryReader binr)
-    {
-        byte bt = 0;
-        byte lowbyte = 0x00;
-        byte highbyte = 0x00;
-        int count = 0;
-        bt = binr.ReadByte();
-        // if (bt != 0x02)
-        //     return 0;
-        bt = binr.ReadByte();
-
-        if (bt == 0x81)
-            count = binr.ReadByte();
-        else
-            if (bt == 0x82)
-        {
-            highbyte = binr.ReadByte();
-            lowbyte = binr.ReadByte();
-            byte[] modint = { lowbyte, highbyte, 0x00, 0x00 };
-            count = BitConverter.ToInt32(modint, 0);
-        }
-        else
-        {
-            count = bt;
-        }
-
-        while (binr.ReadByte() == 0x00)
-        {
-            count -= 1;
-        }
-        binr.BaseStream.Seek(-1, SeekOrigin.Current);
-        return count;
     }
 }
